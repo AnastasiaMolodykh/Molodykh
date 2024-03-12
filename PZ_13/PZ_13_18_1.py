500b023ad0057de9d661cb43627e34eb06344242
molodyha197@gmail.com <molodyha197@gmail.com>
2024-03-13 00:14:10 +0300

    change PZ_13

--- PZ_13/PZ_13_18_1.py
+++ PZ_13/PZ_13_18_1.py
@@ -1,1 +1,16 @@
 # 1. В матрице элементы кратные 3 увеличить в 3 раза
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+def three(num):
+    if num % 3 == 0:
+        return num * 3
+    return num
+
+new_matrix = list(map(lambda row: list(map(three, row)), matrix))
+
+for i in new_matrix:
+    print(i)
--- PZ_13/PZ_13_18_2.py
+++ PZ_13/PZ_13_18_2.py
@@ -1,1 +1,18 @@
 # 2. В матрице найти среднее арифметическое элементов последних двух столбцов.
+
+from functools import reduce
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+two_last = map(lambda row: row[-2:], matrix)
+list_chis = reduce(lambda x, y: x + y, two_last)
+
+sum_el = sum(list_chis)
+kol = len(list_chis)
+sr_ar = sum_el / kol
+
+print("Среднее арифметическое элементов последних двух столбцов:", sr_ar)
