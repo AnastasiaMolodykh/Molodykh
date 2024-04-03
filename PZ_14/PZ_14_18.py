3408eae353a3a1726314758f54e54acf3b098da4
AnastasiaMolodykh <molodyha197@gmail.com>
2024-04-03 14:22:17 +0300

    change PZ_14

--- PZ_14/PZ_14_18.py
+++ PZ_14/PZ_14_18.py
@@ -1,3 +1,13 @@
 # Из исходного текстового файла (expansion.txt) выбрать имена фалов,
 # соответствующие типам: .xls, .xml, .html, .css, .py.Посчитать количество полученных
 # элементов.
+import re
+
+with open('expansion.txt', 'r') as file:
+    data = file.read()
+
+imena = re.findall(r'\b\w+\.(?:xls|xml|html|css|py)\b', data)
+kolvo = len(imena)
+
+print('Имена файлов', imena)
+print('Количество файлов', kolvo)
